4168a54c46e220122c2d5a4b1c4e8bc67f9b166c
Sietse Ringers <mail@sietseringers.net>
2019-08-21 11:54:50 +0200

    refactor: restructure webclient

---
+++ webapp/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euxo pipefail
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 [language]"
+	exit 1
+fi
+
+cd "$(dirname "$0")"
+mkdir -p ./build
+mkdir -p ./build/assets
+
+cp ./$1/index.html ./build/
+cp ./$1/messages.js ./build/assets/
+cp ./common.js ./build/assets/
+cp ./node_modules/bootstrap/dist/css/bootstrap.min.css ./build/assets/
+cp ./node_modules/jquery/dist/jquery.min.js ./build/assets/
+cp ./node_modules/@privacybydesign/irmajs/dist/irma.js ./build/assets/
